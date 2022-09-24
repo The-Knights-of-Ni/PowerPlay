db45c7ddee1193b4c44e0ef624e68d53dac9b2ac
albonec <70042211+albonec@users.noreply.github.com>
2022-09-24 13:50:39 -0700

    control method for bar position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Control.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Control.java
@@ -11,4 +11,21 @@ public class Control extends Subsystem {
     public Control(Telemetry telemetry, HardwareMap hardwareMap, ElapsedTime timer) {
         super(telemetry, hardwareMap, timer);
     }
+
+    public void Extend4Bar() {
+        //extend code here
+    }
+
+    public enum BarPosition {
+        HIGH(0), //TODO: calibrate constants
+        MIDDLE(1),
+        LOW(2)
+
+        public final int position;
+
+        BarPosition(int position) {
+            this.position = position;
+        }
+    }
+
 }
