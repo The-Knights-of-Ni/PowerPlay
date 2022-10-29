0e80d7e47bf6941583a2f1e5597e9e50711df3a2
Ashwin <arihant2math@gmail.com>
2022-10-28 17:09:28 -0700

    added timestamps

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/WebLog.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/WebLog.java
@@ -1,5 +1,9 @@
 package org.firstinspires.ftc.teamcode.Util;
 
+import android.os.Build;
+
+import java.time.LocalDate;
+
 public class WebLog {
     public enum LogSeverity {
         VERBOSE,
@@ -11,10 +15,12 @@ public class WebLog {
     public String TAG;
     public String message;
     public LogSeverity severity;
+    public LocalDate timestamp;
 
     public WebLog(String tag, String message, LogSeverity severity) {
         this.TAG = tag;
         this.message = message;
         this.severity = severity;
+        this.timestamp = LocalDate.now();
     }
 }
