44e1dc186b8bb6d056c34b769b87316861443df9
Ashwin <arihant2math@gmail.com>
2022-12-17 15:28:10 -0800

    Revert "Functional stuff for the allMotorPIDControl function"
    This reverts commit 15e6644406871739aa7f1b6ff854a934021db41d.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoCycleBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoCycleBlue.java
@@ -21,10 +21,7 @@ public class AutoCycleBlue extends Auto {
         initAuto(AllianceColor.BLUE);
         waitForStart();
         timer.reset();
-        robot.drive.moveVector(new Vector(0, -48*mmPerInch));
-        robot.drive.moveVector(new Vector(24*mmPerInch, 48*mmPerInch));
-        robot.drive.moveVector(new Vector(-24*mmPerInch, 0));
-        robot.drive.turnByAngle(90);
+        robot.drive.moveVector(new Vector(24*mmPerInch, 24*mmPerInch));
 //        while (timer.seconds() < 20) {
 //            // TODO: Insert Intake code
 //            robot.drive.moveForward(22 * mmPerInch);
