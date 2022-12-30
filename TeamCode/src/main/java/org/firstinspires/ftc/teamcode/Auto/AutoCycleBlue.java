d7efb2393e2d44f48aea3e289b263373f01ec3f2
albonec <agbonecchi@gmail.com>
2022-12-30 14:02:50 -0800

    auto path stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoCycleBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoCycleBlue.java
@@ -18,18 +18,17 @@ public class AutoCycleBlue extends Auto {
     @Override
     public void runOpMode() throws InterruptedException {
 
-        initAuto(AllianceColor.BLUE);
+        initAuto(AllianceColor.RED);
         waitForStart();
         timer.reset();
-//        telemetry.addData("cone color", robot.vision.detectConeColor());
-        robot.drive.moveVector(new Vector(24*mmPerInch, 44*mmPerInch));
-//        robot.drive.moveForward(12*mmPerInch);
-        robot.drive.turnByAngle(-90);
-//        while (timer.seconds() < 20) {
-//            // TODO: Insert Intake code
-//            robot.drive.moveForward(22 * mmPerInch);
-//            // TODO: Insert Scoring code
-//            robot.drive.moveBackward(22 * mmPerInch);
-//        }
+        telemetry.addData("cone color", robot.vision.detectConeColor());
+        robot.drive.moveForward(24*mmPerInch);
+        robot.drive.moveVector(new Vector(0, 24*mmPerInch), -45);
+        while (timer.seconds() < 20) {
+            // TODO: Insert Intake code
+            robot.drive.moveForward(22 * mmPerInch);
+            // TODO: Insert Scoring code
+            robot.drive.moveBackward(22 * mmPerInch);
+        }
     }
 }
