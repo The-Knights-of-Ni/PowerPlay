7e21af8648ee9709744a857cd3c0eee3ca491df9
albonec <agbonecchi@gmail.com>
2023-01-02 14:12:29 -0800

    AutoCycleRed

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoCycleRedF2.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Subsystems.Vision.ConeColorPipeline.ConeColor;
+import org.firstinspires.ftc.teamcode.Util.AllianceColor;
+import org.firstinspires.ftc.teamcode.Util.Vector;
+
+@Autonomous(name = "Auto Cycle Red F2")
+public class AutoCycleRedF2 extends Auto {
+    /**
+     * Override of runOpMode()
+     *
+     * <p>Please do not swallow the InterruptedException, as it is used in cases where the op mode
+     * needs to be terminated early.</p>
+     *
+     * @see com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+     */
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initAuto(AllianceColor.RED);
+        waitForStart();
+        timer.reset();
+        ConeColor coneColor = robot.vision.detectConeColor();
+        telemetry.addData("Cone Color", coneColor);
+        robot.drive.moveVector(new Vector(0*mmPerInch, 48*mmPerInch), 11.3);
+        while (timer.seconds() < 20) {
+            // TODO: Insert Scoring code
+            robot.drive.moveVector(new Vector(-12*mmPerInch, -24*mmPerInch), -45);
+            // TODO: Insert Intake code
+            robot.drive.moveVector(new Vector(12*mmPerInch, 24*mmPerInch), 45);
+        }
+        switch (coneColor) {
+            case PINK:
+                robot.drive.moveForward(24*mmPerInch);
+            case ORANGE:
+                robot.drive.moveForward(48*mmPerInch);
+        }
+    }
+}
