7f62913236ae96a66f05d1045ad6ddae50bac99a
Ashwin <arihant2math@gmail.com>
2023-01-07 14:51:10 -0800

    high BarState calibrated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Control/Control.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Control/Control.java
@@ -19,7 +19,7 @@ public class Control extends Subsystem {
     private Servo arm;
 
     public enum BarState {
-        HIGH(4275, 1.0), //TODO: calibrate constants
+        HIGH(4430, 1.0), //TODO: calibrate constants
         MIDDLE(3320, 1.0),
         LOW(2430, 1.0),
         PICKUP(0, 1.0);
